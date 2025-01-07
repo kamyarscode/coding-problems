241a48342b1c628bc4c9b2ecade11b32d1a7edb4
kamyar <52869955+kamyarscode@users.noreply.github.com>
2025-01-07 00:07:48 -0700

    counting sort

---
+++ solutions/countingSort1.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Initialize empty array of 100 0s.
+    zero_array = [0] * 100
+
+    # Increase count by 1 at index of numbers seen.
+    for i in range(len(arr)):
+        zero_array[arr[i]] += 1
+
+    return zero_array
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
