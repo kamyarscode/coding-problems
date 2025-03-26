c8915da75b53b7e11649520fd8354040d5bad275
kamyar <52869955+kamyarscode@users.noreply.github.com>
2025-03-26 10:56:21 -0600

    solved number line jumps with kangaroo problem

---
+++ solutions/numberLineJumps.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    # Write your code here
+    
+    # base case check
+    if v2 >= v1:
+        return "NO"
+    
+    # Check for intersection. don't use whole numbers so avoid the '//' operator
+    intersection = (x2 - x1) / (v1 - v2)
+   
+   # Check for intersection, and if present return yes.
+    return "NO" if intersection % 1 != 0 else "YES"
+
+        
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
